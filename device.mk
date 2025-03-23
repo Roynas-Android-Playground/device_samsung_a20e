9bf4c2d547997cd39d66adb57d70deabfee48fad
royna2544 <whiteshell2544@naver.com>
2025-03-23 14:36:25 +0900

    a20e: Init

---
+++ device.mk
@@ -0,0 +1,15 @@
+# Copyright (C) 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/exynos7885-common/exynos7885-common.mk)
+
+# Inherit proprietary files setup
+$(call inherit-product, vendor/samsung/a20e/a20e-vendor.mk)
+
+# Inherit dalvik config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Display resolution
+TARGET_SCREEN_HEIGHT := 1560
+TARGET_SCREEN_WIDTH := 720
