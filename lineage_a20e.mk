7fbab15b28bb2a9bde67371504af75142a5f97ee
royna2544 <whiteshell2544@naver.com>
2025-04-08 22:08:15 +0900

    a20e: Include LineageOS device config

--- lineage_a20e.mk
+++ lineage_a20e.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a20e/device.mk)
 
+# Inherit LineageOS common device config
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 BUILD_FINGERPRINT := "samsung/a20ektt/a20e:11/RP1A.200720.012/A202KKKU8CUL3:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
