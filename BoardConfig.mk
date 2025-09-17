93daac00dbc8c74855dc1191e29006e255681cbc
nhglong <its.nhglong@gmail.com>
2025-09-17 14:20:11 +0000

    a20e: Switch to fragmented defconfigs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,8 @@ DEVICE_PATH := device/samsung/a20e
 TARGET_OTA_ASSERT_DEVICE := a20e,a20edd
 
 # Kernel
-TARGET_KERNEL_CONFIG := exynos7885-a20e_defconfig
+TARGET_KERNEL_CONFIG := exynos7885_defconfig
+TARGET_KERNEL_CONFIG += a20e.config
 
 # Display
 TARGET_SCREEN_DENSITY := 280
